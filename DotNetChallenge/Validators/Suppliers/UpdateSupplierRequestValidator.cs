2ed3e4095e1db090e549bce3dfac63ef94493a0c
hnam04 <hoangnam27062004@gmail.com>
2026-08-10 16:05:03 +0700

    feat(challenge_02): add customer and supplier validator

---
+++ DotNetChallenge/Validators/Suppliers/UpdateSupplierRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using DotNetChallenge.DTOs.Suppliers;
+using FluentValidation;
+
+namespace DotNetChallenge.Validators.Suppliers
+{
+    public class UpdateSupplierRequestValidator
+        : AbstractValidator<UpdateSupplierRequest>
+    {
+        public UpdateSupplierRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Supplier name is required.")
+                .MaximumLength(200)
+                .WithMessage("Supplier name must not exceed 200 characters.");
+
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .When(x => !string.IsNullOrWhiteSpace(x.Email))
+                .WithMessage("Email format is invalid.")
+                .MaximumLength(255)
+                .WithMessage("Email must not exceed 255 characters.");
+
+            RuleFor(x => x.Phone)
+                .MaximumLength(50)
+                .WithMessage("Phone must not exceed 50 characters.");
+
+            RuleFor(x => x.Address)
+                .MaximumLength(500)
+                .WithMessage("Address must not exceed 500 characters.");
+        }
+    }
+}
