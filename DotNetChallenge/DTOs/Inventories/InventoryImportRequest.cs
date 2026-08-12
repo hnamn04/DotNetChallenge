0490941b7e6a019740461adae87d6ebdf8c69862
hnam04 <hoangnam27062004@gmail.com>
2026-08-13 02:02:24 +0700

    feat(challenge_04): add inventory DTO

---
+++ DotNetChallenge/DTOs/Inventories/InventoryImportRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetChallenge.DTOs.Inventories
+{
+    public class InventoryImportRequest
+    {
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public string Note { get; set; } = string.Empty;
+    }
+}
---
+++ DotNetChallenge/DTOs/Inventories/InventoryResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetChallenge.DTOs.Inventories
+{
+    public class InventoryResponse
+    {
+        public Guid ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int ReservedQuantity { get; set; }
+
+        public int AvailableQuantity => Quantity - ReservedQuantity;
+    }
+}
