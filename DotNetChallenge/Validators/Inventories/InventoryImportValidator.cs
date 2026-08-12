846d362aa298c8a8c6cd3a2dd633fb8342eba668
hnam04 <hoangnam27062004@gmail.com>
2026-08-13 02:06:32 +0700

    feat(challenge_04): add inventory validator

---
+++ DotNetChallenge/Validators/Inventories/InventoryImportValidator.cs
@@ -0,0 +1,25 @@
+﻿using DotNetChallenge.DTOs.Inventories;
+using FluentValidation;
+
+namespace DotNetChallenge.Validators.Inventories
+{
+    public class InventoryImportValidator : AbstractValidator<InventoryImportRequest>
+    {
+        public InventoryImportValidator()
+        {
+            RuleFor(x => x.ProductId)
+                .NotEmpty()
+                .WithMessage("ProductId is required.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Quantity must be greater than 0.");
+
+            RuleFor(x => x.Note)
+                .NotEmpty()
+                .WithMessage("Note is required.")
+                .MaximumLength(1000)
+                .WithMessage("Note must not exceed 1000 characters.");
+        }
+    }
+}
