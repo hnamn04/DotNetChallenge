bacd12f66f9bdbd233f8c868306b31d37556b963
hnam04 <hoangnam27062004@gmail.com>
2026-08-14 02:22:41 +0700

    feat(challenge_07): add sales order validator

---
+++ DotNetChallenge/Validators/SalesOrders/CreateSalesOrderValidator.cs
@@ -0,0 +1,36 @@
+﻿using DotNetChallenge.DTOs.SalesOrders;
+using FluentValidation;
+
+namespace DotNetChallenge.Validators.SalesOrders
+{
+    public class CreateSalesOrderValidator : AbstractValidator<CreateSalesOrderRequest>
+    {
+        public CreateSalesOrderValidator()
+        {
+            RuleFor(x => x.CustomerId)
+                .NotEmpty()
+                .WithMessage("CustomerId is required.");
+
+            RuleFor(x => x.Items)
+                .NotEmpty()
+                .WithMessage(
+                    "Sales order must contain at least one item.");
+
+            RuleForEach(x => x.Items)
+                .SetValidator(new SalesOrderItemValidator());
+
+            RuleFor(x => x.Items)
+                .Must(HaveUniqueProducts)
+                .WithMessage("A product cannot appear more than once in a sales order.");
+        }
+
+        // Helper method to check for unique products in the sales order items
+        private static bool HaveUniqueProducts(List<SalesOrderItemRequest> items)
+        {
+            return items
+                .Select(x => x.ProductId)
+                .Distinct()
+                .Count() == items.Count;
+        }
+    }
+}
