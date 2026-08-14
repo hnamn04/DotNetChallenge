f4077d196baf676c83a4255dc0880b924ef1f0d3
hnam04 <hoangnam27062004@gmail.com>
2026-08-14 14:00:49 +0700

    feat(challenge_09): add customer search filter and pagination

---
+++ DotNetChallenge/Validators/Customers/CustomerQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using DotNetChallenge.DTOs.Customers;
+using FluentValidation;
+
+namespace DotNetChallenge.Validators.Customers
+{
+    public class CustomerQueryValidator : AbstractValidator<CustomerQueryRequest>
+    {
+        public CustomerQueryValidator()
+        {
+            RuleFor(x => x.Page)
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Page must be greater than or equal to 1.");
+
+            RuleFor(x => x.Limit)
+                .InclusiveBetween(1, 100)
+                .WithMessage("Limit must be between 1 and 100.");
+        }
+    }
+}
