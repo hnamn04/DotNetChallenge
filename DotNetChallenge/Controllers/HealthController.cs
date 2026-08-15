87f9d86d77b4564a9655a4d866937acd559a2719
hnam04 <hoangnam27062004@gmail.com>
2026-08-15 16:53:56 +0700

    feat(challenge_12): add health controller

---
+++ DotNetChallenge/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using DotNetChallenge.Models.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetChallenge.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet] public ActionResult<ApiResponse<object>> GetHealth() 
+        { 
+            return Ok(new ApiResponse<object>(true, "API is running.", null)); 
+        }
+    }
+}
